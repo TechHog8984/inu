d37bea665332ee24d8f0ca03103223d8e4f66a86
TechHog8984 <lebron.jenroy@gmail.com>
2024-10-08 22:28:10 -0400

    support for program flags & setup for decompiler

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use bytecode::Bytecode;
+use decompiler::{build_decompiler, Decompiler};
 use read::{build_reader, Reader};
 use std::{env, fs, process::exit};
 
 pub mod bytecode;
+pub mod decompiler;
 pub mod read;
+pub mod util;
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
@@ -13,7 +16,30 @@ fn main() {
         display_help();
     }
 
-    let input_path: &String = &argv[1];
+    let mut do_decompile: bool = false;
+    let mut input_path: Option<&String> = None;
+    for i in 1..argv.len() {
+        let value: &String = &argv[i];
+        if value.starts_with("-") {
+            let option: &str = &value[1..value.len()];
+            match option {
+                "dec" => {
+                    do_decompile = true;
+                }
+                _ => {
+                    panic!("unexpected argument {} found", value);
+                }
+            }
+        } else {
+            input_path = Some(value);
+        }
+    }
+
+    if input_path.is_none() {
+        panic!("expected argument file not found");
+    }
+
+    let input_path: &String = input_path.unwrap();
 
     let input_bytes: Vec<u8> =
         fs::read(input_path).expect(format!("Failed to read file at {}", input_path).as_str());
@@ -21,19 +47,20 @@ fn main() {
     let mut reader: Reader = build_reader(&input_bytes);
     let mut bytecode: Bytecode = reader.read();
 
-    /*
-    println!(
-        "version: {:?}, format: {}, endianness: {}, size_int: {}, size_sizet: {}, size_instruction: {}, size_luanumber: {}, luanumber_integral: {}",
-        bytecode.version, bytecode.format, bytecode.endianness, bytecode.size_int, bytecode.size_sizet,
-        bytecode.size_instruction, bytecode.size_luanumber, bytecode.luanumber_integral
-    );
-    println!("main_proto: {:?}", bytecode.main_proto);
-    */
+    if do_decompile {
+        let mut decompiler: Decompiler = build_decompiler(bytecode);
 
+        println!("{}", decompiler.decompile());
+
+        return;
+    }
     bytecode.print_disassembly();
 }
 fn display_help() {
     println!("inu - A disassembler for lua\n");
-    println!("Usage: inu file");
+    println!("Usage: inu [options] file");
+    println!();
+    println!("options:");
+    println!("    -dec : decompile");
     exit(0);
 }
